381d0366de0e1a8965bee26731b967ad8468cfc2
iamsixwater <soup.sixwater@gmail.com>
2022-02-10 21:38:15 +0900

    1_신고결과받기 코드 개선

--- Programmers/1_신고결과받기.py
+++ Programmers/1_신고결과받기.py
@@ -1,5 +1,23 @@
+# 개선 코드
 def solution(id_list, report, k):
+    answer = [0] * len(id_list)
+    reportee = {x: 0 for x in id_list}
     
+    # k번 신고 당한 사람 색출
+    for r in set(report):
+        reportee[r.split()[1]] += 1
+        
+    # 신고자가 받을 메일 개수 도출
+    for r in set(report):
+        if reportee[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+    
+    return answer
+
+
+# 기존 코드
+'''
+def solution(id_list, report, k):
     # (신고당한 사람, [신고한 사람 목록]) 생성
     report_string_set = set(report) # 중복 신고 제거
     report_set = dict()
@@ -33,3 +51,4 @@ def solution(id_list, report, k):
         answer.append(report_num)
     
     return answer
+'''
