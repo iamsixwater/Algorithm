28aca61b104917b1336bcd45e1c28066211aa7d4
iamsixwater <soup.sixwater@gmail.com>
2022-02-14 22:49:27 +0900

    1_숫자 문자열과 영단어 replace 버전 추가

--- Programmers/1_숫자_문자열과_영단어.py
+++ Programmers/1_숫자_문자열과_영단어.py
@@ -14,3 +14,18 @@ def solution(s):
                 str_num = ""
     
     return int(answer)
+
+# replace 사용 버전
+'''
+def solution(s):
+    number_map = {
+        "zero": 0, "one": 1, "two": 2, "three": 3,
+        "four": 4, "five": 5, "six": 6, "seven": 7,
+        "eight": 8, "nine": 9
+    }
+    
+    for key, value in number_map.items():
+        s = s.replace(key, str(value))
+    
+    return int(s)
+'''
