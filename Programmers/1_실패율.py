4ca01c1a7af6a220e772a7355d3ca2d3aad8b12c
iamsixwater <soup.sixwater@gmail.com>
2022-02-20 22:21:42 +0900

    1_실패율 버전 추가

--- Programmers/1_실패율.py
+++ Programmers/1_실패율.py
@@ -1,3 +1,5 @@
+# 단일 반복문으로 실행 시간 짧음 O(n)
+'''
 def solution(N, stages):
     completed = {}
     for n in range(1, N + 2):
@@ -24,3 +26,27 @@ def solution(N, stages):
             answer.append(sorted_dict[n][0])
     
     return answer
+'''
+
+# 이중 반복분으로 실행 시간 오래 걸림 O(n^2)
+def solution(N, stages):
+    result = {}
+    total_user = len(stages)
+    
+    for n in range(1, N + 1):
+        if total_user > 0:
+            n_count = stages.count(n) # O(n)
+            result[n] = n_count / total_user
+            total_user -= n_count
+        else:
+            result[n] = 0
+    
+    return sorted(result, key = lambda x: result[x], reverse = True)
+
+'''
+# sorted
+result => result.keys()랑 같은 뜻(keys 생략 가능)
+key => 정렬을 목적으로 하는 함수(lambda 이용 가능)
+lambda x: result[x] => result[key]가 되므로, value를 기준으로 정렬하겠다는 의미
+'''
+    
