0907b648aa6d7342d8a30b11a276b51d580013e5
iamsixwater <soup.sixwater@gmail.com>
2022-02-22 22:57:28 +0900

    1_수박수박수박수박수박수

---
+++ Programmers/1_수박수박수박수박수박수.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = ""
+    for i in range(1, n + 1):
+        answer += "수" if i % 2 == 1 else "박"
+    
+    return answer
+
+# def solution(n):
+#     return ("수박" * n)[:n]
