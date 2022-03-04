36abbe0765219c2677f7af9415ef741d89e059f3
iamsixwater <soup.sixwater@gmail.com>
2022-03-04 21:17:38 +0900

    폴더 정리

--- Teamnote/2_카펫.py
+++
@@ -1,9 +0,0 @@
-def solution(brown, yellow):
-    answer = []
-    for i in range(1, int(yellow ** 0.5) + 1):
-        if yellow % i == 0:
-            if brown == ((i + 1) * 2 + (yellow//i + 1) * 2):
-                answer.append(yellow // i + 2)
-                answer.append(i + 2)
-    
-    return answer
